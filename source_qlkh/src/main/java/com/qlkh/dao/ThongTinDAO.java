8260c3b0a8ce33427e2b85059f4cdfb50808155a
DuyHoa <duyhoa190202@gmail.com>
2022-06-06 15:05:13 +0700

    commit

---
+++ source_qlkh/src/main/java/com/qlkh/dao/ThongTinDAO.java
@@ -0,0 +1,85 @@
+package com.qlkh.dao;
+
+import com.qlkh.model.JdbcHelper;
+import com.qlkh.model.Sqlbase;
+import com.qlkh.model.Util;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author abc
+ */
+public class ThongTinDAO implements Sqlbase.sqlTT {
+
+    // Xem thông tin bảng điểm
+    public List<Object[]> getBangDiem(Integer maKH) {
+        List<Object[]> list = new ArrayList<>();
+        try {
+            ResultSet rs = JdbcHelper.getResultSet(SQL_PROC_DIEM, maKH);
+            while (rs.next()) {
+                double diem = rs.getDouble(Sqlbase.cols.Diem.name());
+                Object[] x = {rs.getObject(1), rs.getObject(2),
+                    diem, Util.HocLuc(diem)};
+                list.add(x);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return list;
+    }
+
+    // Xem thông tin người học
+    public List<Object[]> getNguoiHoc() {
+        List<Object[]> list = new ArrayList<>();
+        try {
+            ResultSet rs = JdbcHelper.getResultSet(SQL_PROC_NGUOI_HOC);
+            while (rs.next()) {
+                Object[] x = {
+                    rs.getInt(1),
+                    rs.getInt(2),
+                    rs.getDate(3),
+                    rs.getDate(4),};
+                list.add(x);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return list;
+    }
+
+    // Xem thông tin chuyên đề
+    public List<Object[]> getChuyenDe() {
+        List<Object[]> list = new ArrayList<>();
+        try {
+            ResultSet rs = JdbcHelper.getResultSet(SQL_PROC_DIEM_CD);
+            while (rs.next()) {
+                Object[] x = {rs.getObject(1), rs.getObject(2),
+                    rs.getObject(3), rs.getObject(4), rs.getObject(5)};
+                list.add(x);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return list;
+    }
+
+    // Xem thông tin doanh thu
+    public List<Object[]> getDoanhThu(Integer nam) {
+        List<Object[]> list = new ArrayList<>();
+        try {
+            ResultSet rs = JdbcHelper.getResultSet(SQL_PROC_DOANH_THU, nam);
+            while (rs.next()) {
+                Object[] x = {rs.getObject(1), rs.getObject(2), rs.getObject(3),
+                    rs.getObject(4), rs.getObject(5), rs.getObject(6), rs.getObject(7),};
+                list.add(x);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return list;
+    }
+    
+}
---
+++ source_qlkh/src/main/java/com/qlkh/model/Author.java
@@ -0,0 +1,46 @@
+package com.qlkh.model;
+//
+//import EduSys.ClassesDAO.NhanVienDAO;
+import com.qlkh.dao.NhanVienDAO;
+import com.qlkh.entites.NhanVien;
+
+/**
+ *
+ * @author abc
+ */
+public class Author {
+
+    public static NhanVien user = null;
+
+    // Contructer tạo đối tượng người dùng(kiểm tra mã tồn tại)
+    public Author(String maCode) {
+        user = new NhanVienDAO().SelectById(maCode);
+    }
+
+    public static void setUser(NhanVien user) {
+        Author.user = user;
+    }
+
+    public static NhanVien getUser() {
+        return user;
+    }
+
+    // Xóa người dùng trong chương trình
+    public static void clear() {
+        user = null;
+    }
+
+    // Kiểm tra đăng nhập
+    public static boolean isLogin() {
+        return user != null;
+    }
+
+    // Kiểm tra vai trò
+    public static boolean isManager() {
+        if (user != null) {
+            return user.isVaiTro();
+        }
+        return false;
+    }
+
+}
